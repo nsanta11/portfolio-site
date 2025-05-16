a4762afbb9edc39b9d15b21ff5b75ee02c5fdddc
nicolesa <nsantarsiero@collingmedia.com>
2025-05-16 11:15:56 -0400

    updating loading screen

--- js/index.js
+++ js/index.js
@@ -176,61 +176,61 @@ tl.from("#about-me", {
   ease: "power2.out",
 })
 
-// .from(
-//   ".profile-img",
-//   {
-//     x: -50,
-//     autoAlpha: 0,
-//     scale: 0.97,
-//     duration: 1.2,
-//     ease: "power3.out",
-//   },
-//   "-=0.6"
-// );
+.from(
+  ".profile-img",
+  {
+    x: -50,
+    autoAlpha: 0,
+    scale: 0.97,
+    duration: 1.2,
+    ease: "power3.out",
+  },
+  "-=0.6"
+);
 
 // Define functions for custom effects
-const applyCustomEffect_3 = (contentElement) => {
-  // Select relevant elements for the effect
-  const clipPath = contentElement.querySelectorAll("svg clipPath");
-  const poster = contentElement.querySelectorAll(".poster");
-  const posterInner = contentElement.querySelectorAll(".poster__inner");
+// const applyCustomEffect_3 = (contentElement) => {
+//   // Select relevant elements for the effect
+//   const clipPath = contentElement.querySelectorAll("svg clipPath");
+//   const poster = contentElement.querySelectorAll(".poster");
+//   const posterInner = contentElement.querySelectorAll(".poster__inner");
 
-  // Animation logic for each clipPath element
-  [...clipPath].forEach((clipPathEl, pos) => {
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: poster[pos], // Triggers based on poster element
-          start: "top bottom", // Start animation when poster is at top of viewport
-          end: "bottom top", // End animation when poster is at bottom of viewport
-          scrub: true, // Allows smooth scrubbing effect
-        },
-      })
-      .fromTo(
-        clipPathEl,
-        {
-          xPercent: pos === 0 ? 40 : -80, // Initial xPercent value based on position
-        },
-        {
-          ease: "none",
-          xPercent: pos === 0 ? -40 : 20, // Target xPercent value based on position
-        },
-        0
-      )
-      .fromTo(
-        posterInner[pos],
-        {
-          xPercent: pos === 0 ? -5 : 5, // Initial xPercent value based on position
-          yPercent: pos === 0 ? -5 : 5, // Initial yPercent value based on position
-        },
-        {
-          xPercent: pos === 0 ? 5 : -5, // Target xPercent value based on position
-          yPercent: pos === 0 ? 5 : -5, // Target yPercent value based on position
-        },
-        0
-      );
-  });
-};
+//   // Animation logic for each clipPath element
+//   [...clipPath].forEach((clipPathEl, pos) => {
+//     gsap
+//       .timeline({
+//         scrollTrigger: {
+//           trigger: poster[pos], // Triggers based on poster element
+//           start: "top bottom", // Start animation when poster is at top of viewport
+//           end: "bottom top", // End animation when poster is at bottom of viewport
+//           scrub: true, // Allows smooth scrubbing effect
+//         },
+//       })
+//       .fromTo(
+//         clipPathEl,
+//         {
+//           xPercent: pos === 0 ? 40 : -80, // Initial xPercent value based on position
+//         },
+//         {
+//           ease: "none",
+//           xPercent: pos === 0 ? -40 : 20, // Target xPercent value based on position
+//         },
+//         0
+//       )
+//       .fromTo(
+//         posterInner[pos],
+//         {
+//           xPercent: pos === 0 ? -5 : 5, // Initial xPercent value based on position
+//           yPercent: pos === 0 ? -5 : 5, // Initial yPercent value based on position
+//         },
+//         {
+//           xPercent: pos === 0 ? 5 : -5, // Target xPercent value based on position
+//           yPercent: pos === 0 ? 5 : -5, // Target yPercent value based on position
+//         },
+//         0
+//       );
+//   });
+// };
 
 const applyCustomEffect_2 = (contentElement) => {
   const clipPath = contentElement.querySelectorAll("svg clipPath");
@@ -363,312 +363,312 @@ const applyCustomEffect_1 = (contentElement) => {
     );
 };
 
-const applyCustomEffect_4 = (contentElement) => {
-  const clipPath = contentElement.querySelectorAll("svg clipPath");
-  const poster = contentElement.querySelectorAll(".poster");
-  const posterInner = contentElement.querySelectorAll(".poster__inner");
-  const texts = clipPath[0].querySelectorAll("text");
+// const applyCustomEffect_4 = (contentElement) => {
+//   const clipPath = contentElement.querySelectorAll("svg clipPath");
+//   const poster = contentElement.querySelectorAll(".poster");
+//   const posterInner = contentElement.querySelectorAll(".poster__inner");
+//   const texts = clipPath[0].querySelectorAll("text");
 
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: poster[0],
-        start: "top bottom",
-        end: "bottom top",
-        scrub: true,
-      },
-    })
-    .fromTo(
-      clipPath,
-      {
-        xPercent: 160,
-      },
-      {
-        ease: "none",
-        xPercent: -160,
-      },
-      0
-    )
-    .fromTo(
-      posterInner[0],
-      {
-        xPercent: -10,
-        filter: "hue-rotate(190deg) contrast(600%)",
-      },
-      {
-        xPercent: 10,
-        filter: "hue-rotate(0deg) contrast(100%)",
-      },
-      0
-    )
-    .fromTo(
-      texts,
-      {
-        transformOrigin: "50% 100%",
-        scaleY: 0,
-      },
-      {
-        duration: 0.3,
-        ease: "expo.inOut",
-        stagger: 0.05,
-        scaleY: 1,
-      },
-      0
-    )
-    .fromTo(
-      texts,
-      {
-        skewX: -25,
-      },
-      {
-        duration: 0.3,
-        ease: "sine.in",
-        stagger: 0.05,
-        skewX: 25,
-        opacity: 0,
-      },
-      0
-    );
-};
+//   gsap
+//     .timeline({
+//       scrollTrigger: {
+//         trigger: poster[0],
+//         start: "top bottom",
+//         end: "bottom top",
+//         scrub: true,
+//       },
+//     })
+//     .fromTo(
+//       clipPath,
+//       {
+//         xPercent: 160,
+//       },
+//       {
+//         ease: "none",
+//         xPercent: -160,
+//       },
+//       0
+//     )
+//     .fromTo(
+//       posterInner[0],
+//       {
+//         xPercent: -10,
+//         filter: "hue-rotate(190deg) contrast(600%)",
+//       },
+//       {
+//         xPercent: 10,
+//         filter: "hue-rotate(0deg) contrast(100%)",
+//       },
+//       0
+//     )
+//     .fromTo(
+//       texts,
+//       {
+//         transformOrigin: "50% 100%",
+//         scaleY: 0,
+//       },
+//       {
+//         duration: 0.3,
+//         ease: "expo.inOut",
+//         stagger: 0.05,
+//         scaleY: 1,
+//       },
+//       0
+//     )
+//     .fromTo(
+//       texts,
+//       {
+//         skewX: -25,
+//       },
+//       {
+//         duration: 0.3,
+//         ease: "sine.in",
+//         stagger: 0.05,
+//         skewX: 25,
+//         opacity: 0,
+//       },
+//       0
+//     );
+// };
 
-const applyCustomEffect_5 = (contentElement) => {
-  const clipPath = contentElement.querySelectorAll("svg clipPath");
-  const poster = contentElement.querySelectorAll(".poster");
-  const posterInner = contentElement.querySelectorAll(".poster__inner");
-  const texts = clipPath[0].querySelectorAll("text");
+// const applyCustomEffect_5 = (contentElement) => {
+//   const clipPath = contentElement.querySelectorAll("svg clipPath");
+//   const poster = contentElement.querySelectorAll(".poster");
+//   const posterInner = contentElement.querySelectorAll(".poster__inner");
+//   const texts = clipPath[0].querySelectorAll("text");
 
-  gsap
-    .timeline({
-      defaults: {
-        ease: "none",
-      },
-      scrollTrigger: {
-        trigger: poster[0],
-        start: "top bottom",
-        end: "bottom top",
-        scrub: true,
-      },
-    })
-    .fromTo(
-      clipPath,
-      {
-        xPercent: 160,
-      },
-      {
-        xPercent: -160,
-      },
-      0
-    )
-    .fromTo(
-      poster,
-      {
-        transformOrigin: "-50% 50%",
-        rotation: -5,
-      },
-      {
-        rotation: 10,
-      },
-      0
-    )
-    .fromTo(
-      posterInner,
-      {
-        scale: 3,
-      },
-      {
-        scale: 1,
-      },
-      0
-    )
-    .fromTo(
-      texts,
-      {
-        transformOrigin: "0% 50%",
-        rotation: -45,
-        yPercent: -10,
-        scale: 0.1,
-      },
-      {
-        duration: 0.3,
-        ease: "power1",
-        stagger: 0.05,
-        rotation: 0,
-        scale: 1,
-        yPercent: 0,
-        xPercent: 0,
-      },
-      0
-    );
-};
+//   gsap
+//     .timeline({
+//       defaults: {
+//         ease: "none",
+//       },
+//       scrollTrigger: {
+//         trigger: poster[0],
+//         start: "top bottom",
+//         end: "bottom top",
+//         scrub: true,
+//       },
+//     })
+//     .fromTo(
+//       clipPath,
+//       {
+//         xPercent: 160,
+//       },
+//       {
+//         xPercent: -160,
+//       },
+//       0
+//     )
+//     .fromTo(
+//       poster,
+//       {
+//         transformOrigin: "-50% 50%",
+//         rotation: -5,
+//       },
+//       {
+//         rotation: 10,
+//       },
+//       0
+//     )
+//     .fromTo(
+//       posterInner,
+//       {
+//         scale: 3,
+//       },
+//       {
+//         scale: 1,
+//       },
+//       0
+//     )
+//     .fromTo(
+//       texts,
+//       {
+//         transformOrigin: "0% 50%",
+//         rotation: -45,
+//         yPercent: -10,
+//         scale: 0.1,
+//       },
+//       {
+//         duration: 0.3,
+//         ease: "power1",
+//         stagger: 0.05,
+//         rotation: 0,
+//         scale: 1,
+//         yPercent: 0,
+//         xPercent: 0,
+//       },
+//       0
+//     );
+// };
 
-const applyCustomEffect_6 = (contentElement) => {
-  const clipPath = contentElement.querySelectorAll("svg clipPath");
-  const poster = contentElement.querySelectorAll(".poster");
-  const texts = clipPath[0].querySelectorAll("text");
+// const applyCustomEffect_6 = (contentElement) => {
+//   const clipPath = contentElement.querySelectorAll("svg clipPath");
+//   const poster = contentElement.querySelectorAll(".poster");
+//   const texts = clipPath[0].querySelectorAll("text");
 
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: poster[0],
-        start: "top bottom",
-        end: "bottom top",
-        scrub: true,
-      },
-    })
-    .fromTo(
-      clipPath,
-      {
-        xPercent: 20,
-      },
-      {
-        ease: "none",
-        xPercent: -70,
-      },
-      0
-    )
-    .fromTo(
-      texts,
-      {
-        transformOrigin: "150% 50%",
-        scaleX: 0.25,
-        opacity: 0,
-        xPercent: 100,
-      },
-      {
-        duration: 0.15,
-        ease: "power1",
-        stagger: 0.05,
-        scaleX: 1,
-        xPercent: 0,
-        opacity: 1,
-      },
-      0
-    )
-    .fromTo(
-      poster,
-      {
-        filter: "saturate(15)",
-      },
-      {
-        filter: "saturate(0)",
-      },
-      0
-    );
-};
+//   gsap
+//     .timeline({
+//       scrollTrigger: {
+//         trigger: poster[0],
+//         start: "top bottom",
+//         end: "bottom top",
+//         scrub: true,
+//       },
+//     })
+//     .fromTo(
+//       clipPath,
+//       {
+//         xPercent: 20,
+//       },
+//       {
+//         ease: "none",
+//         xPercent: -70,
+//       },
+//       0
+//     )
+//     .fromTo(
+//       texts,
+//       {
+//         transformOrigin: "150% 50%",
+//         scaleX: 0.25,
+//         opacity: 0,
+//         xPercent: 100,
+//       },
+//       {
+//         duration: 0.15,
+//         ease: "power1",
+//         stagger: 0.05,
+//         scaleX: 1,
+//         xPercent: 0,
+//         opacity: 1,
+//       },
+//       0
+//     )
+//     .fromTo(
+//       poster,
+//       {
+//         filter: "saturate(15)",
+//       },
+//       {
+//         filter: "saturate(0)",
+//       },
+//       0
+//     );
+// };
 
-const applyCustomEffect_7 = (contentElement) => {
-  const clipPath = contentElement.querySelectorAll("svg clipPath");
-  const poster = contentElement.querySelectorAll(".poster");
-  const texts = clipPath[0].querySelectorAll("text");
+// const applyCustomEffect_7 = (contentElement) => {
+//   const clipPath = contentElement.querySelectorAll("svg clipPath");
+//   const poster = contentElement.querySelectorAll(".poster");
+//   const texts = clipPath[0].querySelectorAll("text");
 
-  gsap
-    .timeline({
-      defaults: {
-        ease: "none",
-      },
-      scrollTrigger: {
-        trigger: poster[0],
-        start: "top bottom",
-        end: "bottom top",
-        scrub: true,
-      },
-    })
-    .fromTo(
-      contentElement,
-      {
-        filter: "drop-shadow(-100px -20px 5px #6b0c0c)",
-      },
-      {
-        filter: "drop-shadow(100px 20px 20px #2a1554)",
-      },
-      0
-    )
-    .fromTo(
-      clipPath,
-      {
-        xPercent: 140,
-      },
-      {
-        xPercent: -140,
-      },
-      0
-    )
-    .fromTo(
-      texts,
-      {
-        transformOrigin: "0% 50%",
-        rotation: -45,
-        scale: 0,
-        opacity: 0,
-      },
-      {
-        duration: 0.25,
-        ease: "expo.inOut",
-        stagger: 0.03,
-        rotation: 0,
-        scale: 1,
-        xPercent: 0,
-        opacity: 1,
-      },
-      0
-    );
-};
+//   gsap
+//     .timeline({
+//       defaults: {
+//         ease: "none",
+//       },
+//       scrollTrigger: {
+//         trigger: poster[0],
+//         start: "top bottom",
+//         end: "bottom top",
+//         scrub: true,
+//       },
+//     })
+//     .fromTo(
+//       contentElement,
+//       {
+//         filter: "drop-shadow(-100px -20px 5px #6b0c0c)",
+//       },
+//       {
+//         filter: "drop-shadow(100px 20px 20px #2a1554)",
+//       },
+//       0
+//     )
+//     .fromTo(
+//       clipPath,
+//       {
+//         xPercent: 140,
+//       },
+//       {
+//         xPercent: -140,
+//       },
+//       0
+//     )
+//     .fromTo(
+//       texts,
+//       {
+//         transformOrigin: "0% 50%",
+//         rotation: -45,
+//         scale: 0,
+//         opacity: 0,
+//       },
+//       {
+//         duration: 0.25,
+//         ease: "expo.inOut",
+//         stagger: 0.03,
+//         rotation: 0,
+//         scale: 1,
+//         xPercent: 0,
+//         opacity: 1,
+//       },
+//       0
+//     );
+// };
 
-const applyCustomEffect_8 = (contentElement) => {
-  const clipPath = contentElement.querySelectorAll("svg clipPath");
-  const poster = contentElement.querySelectorAll(".poster");
-  const posterInner = contentElement.querySelectorAll(".poster__inner");
+// const applyCustomEffect_8 = (contentElement) => {
+//   const clipPath = contentElement.querySelectorAll("svg clipPath");
+//   const poster = contentElement.querySelectorAll(".poster");
+//   const posterInner = contentElement.querySelectorAll(".poster__inner");
 
-  [...clipPath].forEach((clipPathEl, pos) => {
-    const texts = clipPathEl.querySelectorAll("text");
+//   [...clipPath].forEach((clipPathEl, pos) => {
+//     const texts = clipPathEl.querySelectorAll("text");
 
-    gsap
-      .timeline({
-        defaults: {
-          ease: "none",
-        },
-        scrollTrigger: {
-          trigger: poster[0],
-          start: "top bottom",
-          end: "bottom top",
-          scrub: true,
-        },
-      })
-      .fromTo(
-        clipPathEl,
-        {
-          yPercent: pos ? 30 : 0,
-        },
-        {
-          ease: "none",
-          yPercent: pos ? -30 : 0,
-        },
-        0
-      )
-      .fromTo(
-        texts,
-        {
-          transformOrigin: "50% 0%",
-          scale: 2,
-          opacity: 0,
-          x: pos ? 100 : -100,
-          y: pos ? gsap.utils.random(150, 300) : gsap.utils.random(150, 300),
-          skewX: pos ? 20 : -20,
-        },
-        {
-          duration: 0.4,
-          ease: "power3",
-          stagger: 0.06,
-          scale: 1.3,
-          opacity: 1,
-          x: 0,
-          y: 0,
-          skewX: 0,
-        },
-        0
-      );
-  });
-};
+//     gsap
+//       .timeline({
+//         defaults: {
+//           ease: "none",
+//         },
+//         scrollTrigger: {
+//           trigger: poster[0],
+//           start: "top bottom",
+//           end: "bottom top",
+//           scrub: true,
+//         },
+//       })
+//       .fromTo(
+//         clipPathEl,
+//         {
+//           yPercent: pos ? 30 : 0,
+//         },
+//         {
+//           ease: "none",
+//           yPercent: pos ? -30 : 0,
+//         },
+//         0
+//       )
+//       .fromTo(
+//         texts,
+//         {
+//           transformOrigin: "50% 0%",
+//           scale: 2,
+//           opacity: 0,
+//           x: pos ? 100 : -100,
+//           y: pos ? gsap.utils.random(150, 300) : gsap.utils.random(150, 300),
+//           skewX: pos ? 20 : -20,
+//         },
+//         {
+//           duration: 0.4,
+//           ease: "power3",
+//           stagger: 0.06,
+//           scale: 1.3,
+//           opacity: 1,
+//           x: 0,
+//           y: 0,
+//           skewX: 0,
+//         },
+//         0
+//       );
+//   });
+// };
 
 const applyCustomEffect_9 = (contentElement) => {
   const clipPath = contentElement.querySelectorAll("svg clipPath");
@@ -782,7 +782,7 @@ const init = () => {
 
 // Preloading all images specified by the selector
 Promise.all([
-  preloadImages(".poster__inner"),
+  preloadImages(".load"),
 ]).then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body
   document.body.classList.remove("loading");
